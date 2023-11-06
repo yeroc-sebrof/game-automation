4b75580079bb588d8d958c8a2d6f7ea68b570cc7
Corey <24794972+yeroc-sebrof@users.noreply.github.com>
2023-11-06 23:08:54 +0000

    Update discipleOfHandAutomation.py

--- ff14/discipleOfHandAutomation.py
+++ ff14/discipleOfHandAutomation.py
@@ -1,25 +1,55 @@
 #!/usr/bin/env python
 
+# Summary:
+# This code cannot simply be run out the box. Some assembly required. It currently is loaded with few recipes. You can
+# just get more online or you can learn as you go.
+
+# Remarks:
+# - vgamepad would perhaps work better? It'd give me more control to press the synthesis button
+# - This has a good list of button order but I don't like the rounded int waits: https://ffxivgillionaire.com/crafting-macros
+# - I need a greater check for food around the standard for loop. I don't want a separate thread because I don't want it
+#     to disturb the on-going craft. Just do it between crafts.
+# - It would be good if I could queue up multiple item craft loops. This could allow for further dependency crafting.
+#   - Although that would require a way to make it explicitly craft with HQ dependencies.
+
 from pyautogui import press as keyPress, mouseDown, mouseUp
 from time import sleep
 
 startAnimationWait = 1.25
-abilityWait = 3.25
-stopAnimationWait = 1.25
+additionalButtonWait = 1.55 # This is for some of the actions that have extra animation
+buttonWait = 1.45
+stopAnimationWait = 3.25
 
-howManyTimes = 10
-itemName = "Stone Level 40"
+itemName = "Stone Level 30 > 40"
 
-def make(abilitiesList: list[str]):
-  for b in abilitiesList:
-    # Starting a new item animation
-    sleep(startAnimationWait)
+abilities = {
+  "Great Strides": '4',
+  "Waste Not": '6',
+  "Veneration": '7',
+  "Innovation": '8'
+}
 
+actions = {
+  "Basic Synthesis": '1',
+
+  "Basic Touch": '2',
+  "Standard Touch": '3'
+}
+
+
+def make(buttonList: list[str]):
+  # Starting a new item animation
+  sleep(startAnimationWait)
+
+  for b in buttonList:
     keyPress(b)
-    sleep(abilityWait)
+    if (b in actions.values()):
+      sleep(additionalButtonWait)
+    
+    sleep(buttonWait)
 
-    # Completed product animation
-    sleep(stopAnimationWait)
+  # Completed product animation
+  sleep(stopAnimationWait)
 
 
 def click():
@@ -28,75 +58,75 @@ def click():
   mouseUp()
 
 
-def estimateTotal(iterations: int, name: str, item: dict[str, list[str]]):
+def estimateTotal(iterations: int, name: str, item: list[str]):
+  """
+  This will estimate how long crafting will take. Does not take into account actions vs abilities.
+  """
+
   totalMoves = len(item)
-  timePer = startAnimationWait + (totalMoves * abilityWait) + stopAnimationWait
+  timePer = startAnimationWait + (totalMoves * (buttonWait + additionalButtonWait)) + stopAnimationWait
   totalTimeMins, remainingSeconds = divmod(timePer * totalMoves * iterations, 60)
   totalTimeHours, remainingMins = divmod(totalTimeMins, 60)
 
   print("%d loops of item %s. This will take around %d:%02d:%02d" % (iterations, name, int(totalTimeHours), int(remainingMins), int(remainingSeconds)))
 
 
-class abilities:
-  basicSynthesis = '1'
-  basicTouch = '2'
-  standardTouch = '3'
-  greatStrides = '4'
-  # 5
-  wasteNot = '6'
-  veneration = '7'
-  innovation = '8'
-  # 9
-  # 0
-  # -
-  # =
+def getCost(iterations: int, item: dict[str, int]):
+  print("This will cost around:")
+
+  for k in item.keys():
+    print("- %s: %d" % (k, item[k] * iterations))
+
+  print()
 
 
-goldsmith = {
-  "Stone Level 32": [
-    abilities.wasteNot,
-    abilities.innovation,
-    abilities.basicTouch,
-    abilities.standardTouch,
-    abilities.basicTouch,
-    abilities.standardTouch,
-    abilities.basicTouch,
-    abilities.veneration,
-    abilities.basicSynthesis
-    ],
+items = {
+  "Stone Level 30 > 40": {
+    "moves": [
+      abilities["Waste Not"],
+      abilities["Innovation"],
 
-  "Stone Level 40": [
-    abilities.wasteNot,
-    abilities.innovation,
+      actions["Basic Touch"],
+      actions["Standard Touch"],
+      
+      actions["Basic Touch"],
+      actions["Standard Touch"],
+      
+      actions["Basic Touch"],
 
-    abilities.basicTouch,
-    abilities.standardTouch,
+      abilities["Veneration"],
+      actions["Basic Synthesis"]
+      ],
+      "cost": {
+        "stone": 1,
+        "wind shard": 5
+      }
+  },
+  "Maple": {
+    "moves": [
+      actions["Basic Touch"],
+      actions["Basic Touch"],
 
-    abilities.basicTouch,
-    abilities.standardTouch,
-
-    abilities.basicTouch,
-
-    abilities.veneration,
-    abilities.basicSynthesis
-    ]
+      actions["Basic Synthesis"]
+      ]
+  }
 }
-
-
-blacksmith = {
-}
-
-
-armourer = {
-}
-
-
-leatherworker = {
-}
-
 
 if __name__ == "__main__":
-  estimateTotal(howManyTimes, itemName, goldsmith)
+  # howManyTimes, _ = divmod(21, 3)
+  howManyTimes, _ = divmod(263, items[itemName]["cost"]["wind shard"])
+
+  if ("moves" not in items[itemName].keys()):
+    raise Exception("You need to define the item moves")
+  
+  estimateTotal(howManyTimes, itemName, items[itemName]["moves"])
+
+  if ("cost" in items[itemName].keys()):
+    getCost(howManyTimes, items[itemName]["cost"])
+
+  print ("...")
+
+  sleep(5)
 
   for w in range(3, 0, -1):
     sleep(1)
@@ -108,4 +138,4 @@ if __name__ == "__main__":
     # Left mouse click to press the Synthesis button
     click()
 
-    make(goldsmith[itemName])
+    make(items[itemName]["moves"])
